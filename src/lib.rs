3efa7dfa24d55aa3099b50b89ba1961e3333b5ca
Thomas Lindae <thomas.lindae@in.tum.de>
2021-02-25 20:09:38 +0100

    renamed crate to throbber; added more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-//! This crate serves as an alternative to [loading](https://crates.io/crates/loading). It is used to display a loading style animation in the terminal while other calculations are done in the main program.
+//! This crate serves as an alternative to [loading](https://crates.io/crates/loading). It is used to display a throbber animation in the terminal while other calculations are done in the main program.
 //!
 //! # Usage
 //! First, add this to your Cargo.toml
 //! ```toml
 //! [dependencies]
-//! loading_rs = "0.1"
+//! throbber = "0.1"
 //! ```
 
 use std::io::Write;
@@ -12,56 +12,63 @@ use std::sync::mpsc::{self, Receiver, Sender, TryRecvError};
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
+/// `⠋   ⠙   ⠹   ⠸   ⠼   ⠴   ⠦   ⠧   ⠇   ⠏`
 pub const DEFAULT_F: [&str; 10] = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+/// `◐   ◓   ◑   ◒`
 pub const CIRCLE_F: [&str; 4] = ["◐", "◓", "◑", "◒"];
+/// `|   /   -   \`
 pub const ROTATE_F: [&str; 4] = ["|", "/", "-", "\\"];
+/// `[=  ]   [ = ]   [  =]   [ = ]`
 pub const MOVE_EQ_F: [&str; 4] = ["[=  ]", "[ = ]", "[  =]", "[ = ]"];
+/// `[-  ]   [ - ]   [  -]   [ - ]`
 pub const MOVE_MIN_F: [&str; 4] = ["[-  ]", "[ - ]", "[  -]", "[ - ]"];
+/// `[=    ]   [==   ]   [ ==  ]   [  == ]   [   ==]   [    =]`
 pub const MOVE_EQ_LONG_F: [&str; 10] = [
     "[=    ]", "[==   ]", "[ ==  ]", "[  == ]", "[   ==]", "[    =]", "[   ==]", "[  == ]",
     "[ ==  ]", "[==   ]",
 ];
+/// `[-    ]   [--   ]   [ --  ]   [  -- ]   [   --]   [    -]`
 pub const MOVE_MIN_LONG_F: [&str; 10] = [
     "[-    ]", "[--   ]", "[ --  ]", "[  -- ]", "[   --]", "[    -]", "[   --]", "[  -- ]",
     "[ --  ]", "[--   ]",
 ];
 
-/// Representation of a loading animation. It can strart, succeed, fail or end at any point.
+/// Representation of a throbber animation. It can strart, succeed, fail or end at any point.
 ///
-/// Note that a call to `end()` takes ownership of the struct and drops it, as such it should be called to completely remove the loading animtion object. If you want to start another animation afterwards, you have to create a new `Loading` object. This is done because multiple calls to start do not actually create multiple threads, instead a call to a *finish function* (like `succeed()`) simply parks the thread and a following `start()` call unparks that thread again. As such, a call to `end()` kills the thread entirely. If you want to just stop the animation, but potentially start it again later on, use `finish()` instead.
+/// Note that a call to `end()` takes ownership of the struct and drops it, as such it should be called to completely remove the throbber animtion object. If you want to start another animation afterwards, you have to create a new `Throbber` object. This is done because multiple calls to start do not actually create multiple threads, instead a call to a *finish function* (like `succeed()`) simply parks the thread and a following `start()` call unparks that thread again. As such, a call to `end()` kills the thread entirely. If you want to just stop the animation, but potentially start it again later on, use `finish()` instead.
 ///
 /// # Examples
 /// ```rust
-/// use loading_rs::Loading;
+/// use throbber::Throbber;
 /// use std::thread;
 /// use std::time::Duration;
 ///
-/// let mut loading = Loading::new()
+/// let mut throbber = Throbber::new()
 ///     .message("calculating stuff".to_string())
 ///     .interval(Duration::from_millis(50))
-///     .frames(&loading_rs::ROTATE_F);
+///     .frames(&throbber::ROTATE_F);
 ///
-/// loading.start();
+/// throbber.start();
 ///
 /// // do stuff
 /// thread::sleep(Duration::from_secs(5));
 ///
-/// loading.success("calculation successful".to_string());
-/// loading.end();
+/// throbber.success("calculation successful".to_string());
+/// throbber.end();
 /// ```
-pub struct Loading {
-    anim: Option<LoadingAnim>,
+pub struct Throbber {
+    anim: Option<ThrobberAnim>,
     message: String,
     interval: Duration,
     frames: &'static [&'static str],
 }
 
-struct LoadingAnim {
+struct ThrobberAnim {
     thread: JoinHandle<()>,
-    sender: Sender<LoadingSignal>,
+    sender: Sender<ThrobberSignal>,
 }
 
-enum LoadingSignal {
+enum ThrobberSignal {
     Start,
     Finish,
     Succ(String),
@@ -72,8 +79,8 @@ enum LoadingSignal {
     End,
 }
 
-impl Loading {
-    /// Creates a new Loading object.
+impl Throbber {
+    /// Creates a new Throbber object.
     pub fn new() -> Self {
         Self {
             anim: None,
@@ -85,7 +92,7 @@ impl Loading {
 
     /// Sets the message that's supposed to print.
     ///
-    /// This does nothing if `start()` was called before. To change the message after `start()` was called, use [`change_message`](Loading::change_message) instead.
+    /// This does nothing if `start()` was called before. To change the message after `start()` was called, use [`change_message`](Throbber::change_message) instead.
     pub fn message(mut self, msg: String) -> Self {
         self.message = msg;
         self
@@ -93,7 +100,7 @@ impl Loading {
 
     /// Sets the interval in which the animation frames are supposed to print.
     ///
-    /// This does nothing if `start()` was called before. To change the interval after `start()` was called, use [`change_interval`](Loading::change_interval) instead.
+    /// This does nothing if `start()` was called before. To change the interval after `start()` was called, use [`change_interval`](Throbber::change_interval) instead.
     pub fn interval(mut self, interval: Duration) -> Self {
         self.interval = interval;
         self
@@ -101,7 +108,7 @@ impl Loading {
 
     /// Sets the animation frames that are supposed to print.
     ///
-    /// This does nothing if `start()` was called before. To change the animation frames after `start()` was called, use [`change_frames`](Loading::change_frames) instead.
+    /// This does nothing if `start()` was called before. To change the animation frames after `start()` was called, use [`change_frames`](Throbber::change_frames) instead.
     pub fn frames(mut self, frames: &'static [&'static str]) -> Self {
         self.frames = frames;
         self
@@ -109,10 +116,12 @@ impl Loading {
 
     /// Changes the message that's supposed to print.toml
     ///
-    /// Unlike [`message`](Loading::message), this will work both before and after `start()` was called.
+    /// Unlike [`message`](Throbber::message), this will work both before and after `start()` was called.
     pub fn change_message(&mut self, msg: String) {
         if let Some(ref anim) = self.anim {
-            anim.sender.send(LoadingSignal::ChMsg(msg.clone())).unwrap();
+            anim.sender
+                .send(ThrobberSignal::ChMsg(msg.clone()))
+                .unwrap();
             anim.thread.thread().unpark();
         }
         self.message = msg;
@@ -120,10 +129,10 @@ impl Loading {
 
     /// Changes the interval in which the animation frames are supposed to print.
     ///
-    /// Unlike [`interval`](Loading::interval), this will work both before and after `start()` was called.
+    /// Unlike [`interval`](Throbber::interval), this will work both before and after `start()` was called.
     pub fn change_interval(&mut self, interval: Duration) {
         if let Some(ref anim) = self.anim {
-            anim.sender.send(LoadingSignal::ChInt(interval)).unwrap();
+            anim.sender.send(ThrobberSignal::ChInt(interval)).unwrap();
             anim.thread.thread().unpark();
         }
         self.interval = interval;
@@ -131,10 +140,10 @@ impl Loading {
 
     /// Changes the animation frames that are supposed to print.
     ///
-    /// Unlike [`frames`](Loading::frames), this will work both before and after `start()` was called.
+    /// Unlike [`frames`](Throbber::frames), this will work both before and after `start()` was called.
     pub fn change_frames(&mut self, frames: &'static [&'static str]) {
         if let Some(ref anim) = self.anim {
-            anim.sender.send(LoadingSignal::ChFrames(frames)).unwrap();
+            anim.sender.send(ThrobberSignal::ChFrames(frames)).unwrap();
             anim.thread.thread().unpark();
         }
         self.frames = frames;
@@ -145,24 +154,25 @@ impl Loading {
     /// If this is the first call to start(), a new thread gets created to play the animation. Otherwise the thread that already exists gets unparked and starts the animation again.
     pub fn start(&mut self) {
         if let Some(ref anim) = self.anim {
-            anim.sender.send(LoadingSignal::Start).unwrap();
+            anim.sender.send(ThrobberSignal::Start).unwrap();
             anim.thread.thread().unpark();
             return;
         }
 
-        let (sender, receiver): (Sender<LoadingSignal>, Receiver<LoadingSignal>) = mpsc::channel();
+        let (sender, receiver): (Sender<ThrobberSignal>, Receiver<ThrobberSignal>) =
+            mpsc::channel();
 
         let msg = self.message.clone();
         let frames = self.frames;
         let interval = self.interval;
         let thread = thread::spawn(move || animation_thread(receiver, msg, frames, interval));
 
-        self.anim = Some(LoadingAnim { thread, sender });
+        self.anim = Some(ThrobberAnim { thread, sender });
     }
 
     /// Starts the animation with the specified `msg`.
     ///
-    /// Equivalent to `loading.change_message(msg); loading.start();`.
+    /// Equivalent to `throbber.change_message(msg); throbber.start();`.
     pub fn start_with_msg(&mut self, msg: String) {
         self.change_message(msg);
         self.start();
@@ -171,7 +181,7 @@ impl Loading {
     /// Stops the current animation, leaving a blank line.
     pub fn finish(&mut self) {
         if let Some(ref anim) = self.anim {
-            anim.sender.send(LoadingSignal::Finish).unwrap();
+            anim.sender.send(ThrobberSignal::Finish).unwrap();
             anim.thread.thread().unpark();
         }
     }
@@ -179,7 +189,7 @@ impl Loading {
     /// Stops the current animation and prints `msg` as a *success message*.
     pub fn success(&mut self, msg: String) {
         if let Some(ref anim) = self.anim {
-            anim.sender.send(LoadingSignal::Succ(msg)).unwrap();
+            anim.sender.send(ThrobberSignal::Succ(msg)).unwrap();
             anim.thread.thread().unpark();
         } else {
             println!("\x1B[2K\r✔ {}", msg);
@@ -189,17 +199,17 @@ impl Loading {
     /// Stops the current animation and prints `msg` as a *fail message*.
     pub fn fail(&mut self, msg: String) {
         if let Some(ref anim) = self.anim {
-            anim.sender.send(LoadingSignal::Fail(msg)).unwrap();
+            anim.sender.send(ThrobberSignal::Fail(msg)).unwrap();
             anim.thread.thread().unpark();
         } else {
             println!("\x1B[2K\r✖ {}", msg);
         }
     }
 
-    /// Encds the animation thread and drops the loading object. If you want to stop the animation without dropping the loading object, use [`finish`](Loading::finish) instead.
+    /// Encds the animation thread and drops the throbber object. If you want to stop the animation without dropping the throbber object, use [`finish`](Throbber::finish) instead.
     pub fn end(mut self) {
         if let Some(anim) = self.anim.take() {
-            anim.sender.send(LoadingSignal::End).unwrap();
+            anim.sender.send(ThrobberSignal::End).unwrap();
             anim.thread.thread().unpark();
             anim.thread.join().unwrap();
         }
@@ -207,7 +217,7 @@ impl Loading {
 }
 
 fn animation_thread(
-    receiver: Receiver<LoadingSignal>,
+    receiver: Receiver<ThrobberSignal>,
     mut msg: String,
     mut frames: &'static [&'static str],
     mut interval: Duration,
@@ -216,40 +226,40 @@ fn animation_thread(
     let mut frame = 0;
     loop {
         match receiver.try_recv() {
-            Ok(LoadingSignal::Start) => {
+            Ok(ThrobberSignal::Start) => {
                 play_anim = true;
                 continue;
             }
-            Ok(LoadingSignal::Finish) => {
+            Ok(ThrobberSignal::Finish) => {
                 print!("\x1B[2K\r");
                 std::io::stdout().flush().unwrap();
                 play_anim = false;
                 continue;
             }
-            Ok(LoadingSignal::Succ(succ_msg)) => {
+            Ok(ThrobberSignal::Succ(succ_msg)) => {
                 println!("\x1B[2K\r✔ {}", succ_msg);
                 play_anim = false;
                 continue;
             }
-            Ok(LoadingSignal::Fail(fail_msg)) => {
+            Ok(ThrobberSignal::Fail(fail_msg)) => {
                 println!("\x1B[2K\r✖ {}", fail_msg);
                 play_anim = false;
                 continue;
             }
-            Ok(LoadingSignal::ChMsg(new_msg)) => {
+            Ok(ThrobberSignal::ChMsg(new_msg)) => {
                 msg = new_msg;
                 continue;
             }
-            Ok(LoadingSignal::ChInt(new_dur)) => {
+            Ok(ThrobberSignal::ChInt(new_dur)) => {
                 interval = new_dur;
                 continue;
             }
-            Ok(LoadingSignal::ChFrames(new_frames)) => {
+            Ok(ThrobberSignal::ChFrames(new_frames)) => {
                 frames = new_frames;
                 frame = 0;
                 continue;
             }
-            Ok(LoadingSignal::End) => {
+            Ok(ThrobberSignal::End) => {
                 break;
             }
             Err(TryRecvError::Disconnected) => {
